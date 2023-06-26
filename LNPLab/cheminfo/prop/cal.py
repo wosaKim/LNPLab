6309c2868c0cdee64fed975cb40775e01e62dee3
wosaKim <william@lnpsolution.com>
2023-06-26 22:58:41 +0900

    desc added

--- LNPLab/cheminfo/prop/cal.py
+++ LNPLab/cheminfo/prop/cal.py
@@ -1,6 +1,8 @@
+from rdkit.Chem import Descriptors, MolFromSmiles
+from rdkit.ML.Descriptors import MoleculeDescriptors
 from padelpy import from_smiles
 
-# calculate molecular descriptors for propane
+# calculate molecular descriptors for propane\
 descriptors = from_smiles('CCC')
 
 # calculate molecular descriptors for propane and butane
@@ -10,10 +12,33 @@ descriptors = from_smiles('CCC')
 desc_fp = from_smiles('CCC', fingerprints=True)
 
 # only calculate fingerprints
-fingerprints = from_smiles('CCC', fingerprints=True, descriptors=False)
-
+fingerprints = from_smiles(
+    'CC1=C(C=C(C=C1)NC(=O)C2=CC=C(C=C2)CN3CCN(CC3)C)NC4=NC=CC(=N4)C5=CN=CC=C5',
+    fingerprints=True,
+    descriptors=False
+)
 # setting the number of threads, this uses one cpu thread to compute descriptors
 # descriptors = from_smiles(['CCC', 'CCCC'], threads=1)
 
 # save descriptors to a CSV file
-_ = from_smiles('CCC', output_csv='descriptors.csv')
+_ = from_smiles(
+    'CC1=C(C=C(C=C1)NC(=O)C2=CC=C(C=C2)CN3CCN(CC3)C)NC4=NC=CC(=N4)C5=CN=CC=C5',
+    fingerprints=True,
+    output_csv='descriptors.csv'
+)
+
+calc = MoleculeDescriptors.MolecularDescriptorCalculator(
+    [x[0] for x in Descriptors._descList]
+)
+
+desc_list = [x[0] for x in Descriptors._descList]
+rdkit_desc = calc.CalcDescriptors(
+    MolFromSmiles(
+        'CC1=C(C=C(C=C1)NC(=O)C2=CC=C(C=C2)CN3CCN(CC3)C)NC4=NC=CC(=N4)C5=CN=CC=C5'
+    )
+)
+
+for desc, value in zip(desc_list, rdkit_desc):
+    print(desc, value)
+
+print(len(desc_list))
